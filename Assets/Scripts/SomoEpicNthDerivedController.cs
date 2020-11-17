9a91114bddbd002b7d20c3a478bfa4d74d5f86ec
Löffler Levente <levente.loffler99@gmail.com>
2020-11-17 18:30:36 +0100

    First commit.

---
+++ Assets/Scripts/SomoEpicNthDerivedController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+public class SomoEpicNthDerivedController : MonoBehaviour
+{
+    public Vector2d[] derivatives;
+    public double[] drags;
+
+
+    private void FixedUpdate()
+    {
+        derivatives[derivatives.Length -2 ] = Vector2d.MoveTowards(derivatives[derivatives.Length - 2], new Vector2d(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * drags[drags.Length - 1], derivatives[derivatives.Length - 1].magnitude * Time.deltaTime);
+
+        for (int i = derivatives.Length - 3; i >= 0; i--)
+        {
+            derivatives[i] = Vector2d.MoveTowards(derivatives[i], Vector2d.zero, derivatives[i].magnitude * Time.deltaTime * drags[i]);
+            derivatives[i] += derivatives[i + 1] * Time.deltaTime;
+        }
+
+        transform.Translate((Vector3)(Vector3d)(derivatives[0] * Time.deltaTime));
+    }
+}
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+
+public class UI : MonoBehaviour
+{
+    private UnityEngine.UI.Text infoBox;
+    private System.Text.StringBuilder stringBuilder;
+    private SomoEpicNthDerivedController somo;
+
+
+    private void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        
+        infoBox = GameObject.Find("InfoBox").GetComponent<UnityEngine.UI.Text>();
+        stringBuilder = new System.Text.StringBuilder();
+        somo = FindObjectOfType<SomoEpicNthDerivedController>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        stringBuilder.Clear();
+        stringBuilder.Append(1 / Time.deltaTime).Append(" update/s").Append(System.Environment.NewLine).Append(System.Environment.NewLine);
+
+        for (int i = 0; i < somo.derivatives.Length; i++)
+            stringBuilder.Append("Derivative ").Append(i + 1).Append(": (").Append(somo.derivatives[i].x).Append(", ").Append(somo.derivatives[i].y).Append(")").Append(System.Environment.NewLine).Append("Drag: ").Append(somo.drags[i]).Append(System.Environment.NewLine).Append(System.Environment.NewLine);
+
+        infoBox.text = stringBuilder.ToString();
+    }
+
+
+    public void FuckGoBack()
+    {
+        somo.transform.position = Vector3.zero;
+    }
+}
